2d3c6a4238d2a9bbebf0e9471702dd2b3d701798
georgehans1 <georgehans301@gmail.com>
2022-03-17 08:17:11 +0000

    Added all necessary routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SearchComponent } from './components/pages/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch: 'full'},
+  {path:'home',component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'search', component:SearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
